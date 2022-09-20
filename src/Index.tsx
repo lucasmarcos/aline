6a6a42ceed15407dd8958b2696c8df518b407b93
Lucas Marcos <lucasmarcos@live.com>
2022-09-20 17:44:06 -0300

    wifi

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,22 +1,22 @@
-const Link = ({ page, children }) => {
+const Link = ({ page, color = "", children }) => {
   return (
-    <div>
-      <a className="underline text-xl" href={page}>{children}</a>
+    <div className={`p-3 ${color}`}>
+      <a className={`underline text-xl ${color === "bg-black" ? "text-white":""}`} href={page}>{children}</a>
     </div>
   );
 };
 
 export const Index = () => {
   return (
-    <div className="m-20">
+    <div className="text-center">
       <Link page="definicao.html">Emoções, difinições e conceitos</Link>
       <Link page="social.html">Habilidades sociais</Link>
       <Link page="final.html">Emoções, parte final</Link>
-      <Link page="alegria.html">Alegria</Link>
-      <Link page="medo.html">Medo</Link>
-      <Link page="nojo.html">Nojo</Link>
-      <Link page="raiva.html">Raiva</Link>
-      <Link page="tristeza.html">Tristeza</Link>
+      <Link page="alegria.html" color="bg-yellow-300">Alegria</Link>
+      <Link page="medo.html" color="bg-black">Medo</Link>
+      <Link page="nojo.html" color="bg-green-500">Nojo</Link>
+      <Link page="raiva.html" color="bg-red-500">Raiva</Link>
+      <Link page="tristeza.html" color="bg-blue-500">Tristeza</Link>
     </div>
   );
 };
