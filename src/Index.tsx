a353086786d14e02fef2d57a0789e7be0db8167b
Lucas Marcos <lucasmarcos@live.com>
2022-09-21 15:13:34 -0300

    workflows

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,3 +1,5 @@
+import { Logo } from "./Logo.js";
+
 const Link = ({ page, color = "", children }) => {
   return (
     <div className={`p-3 ${color}`}>
@@ -9,6 +11,7 @@ const Link = ({ page, color = "", children }) => {
 export const Index = () => {
   return (
     <div className="text-center">
+      <Logo/>
       <Link page="definicao.html">Emoções, difinições e conceitos</Link>
       <Link page="social.html">Habilidades sociais</Link>
       <Link page="final.html">Emoções, parte final</Link>
