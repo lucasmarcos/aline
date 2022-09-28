281be664e8152c2cbeefe251a1f826d46cc189a9
Lucas Marcos <lucasmarcos@live.com>
2022-09-28 16:37:18 -0300

    style

--- src/Template.tsx
+++ src/Template.tsx
@@ -1,13 +1,11 @@
-export const Template = ({ title, children }) => {
-  return (
-    <html lang="pt">
-      <head>
-        <title>{title}</title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="out.css" />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
-};
+export const Template = ({ title, children }) => (
+  <html lang="pt">
+    <head>
+      <title>{title}</title>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" href="out.css" />
+    </head>
+    <body>{children}</body>
+  </html>
+);
--- src/Util.tsx
+++ src/Util.tsx
@@ -1,31 +1,23 @@
-export const Article = ({ children }) => {
-  return <article>{children}</article>;
-};
+export const Article = ({ children }) => <article>{children}</article>;
 
-export const ArticleHeader = ({ className, title, subtitle }) => {
-  return (
-    <div className={className}>
-      <div className="text-center p-7">
-        <h1 className="text-7xl p-1">{title}</h1>
-        <h2 className="text-4xl p-1">{subtitle}</h2>
-      </div>
+export const ArticleHeader = ({ className, title, subtitle }) => (
+  <div className={className}>
+    <div className="text-center p-7">
+      <h1 className="text-7xl p-1">{title}</h1>
+      <h2 className="text-4xl p-1">{subtitle}</h2>
     </div>
-  );
-};
+  </div>
+);
 
-export const P = ({ children }) => {
-  return <p className="p-1 text-lg">{children}</p>;
-};
+export const P = ({ children }) => <p className="p-1 text-lg">{children}</p>;
 
-export const Refs = ({ children }) => {
-  return (
-    <div>
-      <p>Referências</p>
-      {children}
-    </div>
-  );
-};
+export const Refs = ({ children }) => (
+  <div>
+    <p>Referências</p>
+    {children}
+  </div>
+);
 
-export const Content = ({ children }) => {
-  return <main className="container py-6 mx-auto w-1/2">{children}</main>;
-};
+export const Content = ({ children }) => (
+  <main className="container py-6 mx-auto w-1/2">{children}</main>
+);
