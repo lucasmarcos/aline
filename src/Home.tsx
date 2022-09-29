11f3faaf6436c9b8d44e320e7e9f88c67fe26678
Lucas Marcos <lucasmarcos@live.com>
2022-09-29 20:10:55 -0300

    something i guess....

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,17 +6,23 @@ const URLS = {
   Raiva: "raiva.html",
 };
 
+const Title = () => (
+  <div className="text-7xl text-center p-12 bg-black text-white">
+    Psicoeducação na universidade
+  </div>
+);
+
 const Emocao = ({ className, nome, emoji }) => (
   <a href={URLS[nome]}>
     <div className={`${className} p-7`}>
       <div className="text-center text-3xl">{nome}</div>
-      <div className="text-center text-7xl">{emoji}</div>
+      <div className="text-center text-7xl my-3">{emoji}</div>
     </div>
   </a>
 );
 
 const Emocoes = () => (
-  <div className="flex align-center justify-center">
+  <div className="flex align-center justify-center bg-gray-600 flex-col md:flex-row">
     <Emocao className="bg-yellow-300" nome="Alegria" emoji="😊" />
     <Emocao className="bg-red-500" nome="Raiva" emoji="😠" />
     <Emocao className="bg-black text-white" nome="Medo" emoji="😨" />
@@ -27,15 +33,31 @@ const Emocoes = () => (
 
 export const Home = () => (
   <div>
-    <div className="text-7xl text-center m-12">
-      Psicoeducação na universidade
+    <Title />
+    <div className="flex bg-gray-300">
+      <div className="text-xl mx-auto max-w-md py-5">
+        Texto: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat.
+      </div>
+
+      <div className="text-xl mx-auto max-w-md py-5">
+        Trabalhar as emoções: Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
     </div>
-    <div>Texto</div>
-    <div>Trabalhar as emoções</div>
-    <div>Vamos aprender mais?</div>
+
+    <div className="text-center py-3 text-xl bg-gray-600 text-white">
+      Vamos aprender mais? call to action
+    </div>
 
     <Emocoes />
 
-    <div>Agora já leu, clique aqui.</div>
+    <div className="text-center py-3 text-xl bg-gray-600 text-white">
+      Agora já leu, clique aqui. call to action 2
+    </div>
   </div>
 );
