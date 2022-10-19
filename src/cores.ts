1c5b6c6327e478f4480b1aea9ebc28bcc6d6697b
Lucas Marcos <lucasmarcos@live.com>
2022-10-19 16:13:17 -0300

    cores

---
+++ src/cores.ts
@@ -0,0 +1,8 @@
+export const Rosa = "ED6A5A";
+export const Amarelo = "FFBA0A";
+export const CinzaClaro = "9BC1BC";
+export const CinzaEscuro = "5D576B";
+export const Preto = "1D1E1E";
+
+export const BG = (cor) => `bg-[#${cor}]`;
+export const TEXT = (cor) => `text-[#${cor}]`;
